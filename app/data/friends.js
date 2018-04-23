3413925a077b4be142eab268a0dadf7507fb2522
EvelynCude <ebenitez@email.arizona.edu>
2018-04-22 21:41:51 -0700

    fixed math for friend match

--- app/data/friends.js
+++ app/data/friends.js
@@ -20,7 +20,56 @@ var friendsArray =[
         1,
         1
     ]
-}
+},
+{
+    "name": "Tyrion Lannister",
+    "photo": "http://i.dailymail.co.uk/i/pix/2014/02/10/article-2555555-1B582EBA00000578-944_634x577.jpg",
+    "scores": [
+        3,
+        1,
+        3,
+        3,
+        2,
+        1,
+        5,
+        3,
+        4,
+        2
+    ]
+},
+{
+    "name": "Jon Snow",
+    "photo": "https://hips.hearstapps.com/esquireuk.cdnds.net/17/35/2048x2048/square-1504428478-who-jon-snow-mother-game-thrones-2.png?resize=768:*",
+    "scores": [
+        4,
+        3,
+        4,
+        5,
+        3,
+        1,
+        5,
+        4,
+        2,
+        4
+    ]
+},
+{
+    "name": "Daenerys Targaryen",
+    "photo": "https://www.google.com/search?q=khaleesi&rlz=1C1CHBF_enUS771US771&source=lnms&tbm=isch&sa=X&ved=0ahUKEwjr85LZv8_aAhWhwVkKHcHqAw4Q_AUICigB&biw=1284&bih=603#imgrc=VRTdw-hnOg9bvM:",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        2,
+        2,
+        5,
+        2,
+        1,
+        1
+    ]
+    }
+
 ];
 
 //  Export array so that it's accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -34,28 +34,30 @@ module.exports = function(app){
       
       // Loop through friends list and loop through scores of each friends list
       for (var i=0; i<friendsData.length; i++){
+        scoreDifference = 0;
         for (var j=0; j<friendsData[i].scores.length; j++){
-        // test loop
-        console.log(friendsData[i].scores[j]); 
-        console.log("-"+userScores[j]);
-        console.log("-----");
-               
-      // Compute the difference for each survey question between the user's scores and the current loop friend by using another loop
+          // test loop
+          console.log(friendsData[i].scores[j]); 
+          console.log("-"+userScores[j]);
+          console.log("-----");
+          // Compute the difference for each survey question between the user's scores and the current loop friend by using another loop
           scoreDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendsData[i].scores[j]));
           // test math
           console.log(scoreDifference +"\n");
-      // Make/override new matched friend if score difference is smaller than previous matched score
-          if (scoreDifference < matchedFriend.matchScoreDiff){
-            matchedFriend.matchName = friendsData[i].name;
-            matchedFriend.matchPhoto = friendsData[i].photo;
-            matchedFriend.matchScoreDiff = scoreDifference;
-          }  
-        } 
+          console.log(matchedFriend.matchScoreDiff + "matched diff\n");
+        }    
+
+        // Compare the score differences
+        if (scoreDifference < matchedFriend.matchScoreDiff){
+          // Override with new matched friend if score difference is smaller than previous matched score
+          matchedFriend.matchName = friendsData[i].name;
+          matchedFriend.matchPhoto = friendsData[i].photo;
+          matchedFriend.matchScoreDiff = scoreDifference;
+        }  
       }
       friendsData.push(newUser);
       console.log(matchedFriend);
+      // Response with matchedFriend JSON data
       res.json(matchedFriend);
-      
-
     });
 };
